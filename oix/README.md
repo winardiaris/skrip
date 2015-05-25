584af0043bd692d19f4c540cbaa97428a133b4cb
winardiaris <ariswinardy@gmail.com>
2015-05-25 20:07:07 +0700

    menambahkan skrip zsync

--- oix/README.md
+++ oix/README.md
@@ -1,5 +1,3 @@
 # skrip untuk zsync jahitan blankon tambora
-cara pakai
- ./zsync.sh[spasi]tanggal jahitan[spasi]arsitektur
 
-contoh: ./zsync.sh 20150525 i386
+contoh: ./zsync.sh i386
---
+++ oix/coi.sh
@@ -0,0 +1,23 @@
+export archi="$1"
+
+echo "menghapus berkas lama .............. "
+rm -rf *sum*
+zsync "http://cdimage.blankonlinux.or.id/blankon/livedvd-harian/current/tambora-desktop-$archi.iso.zsync"
+wget "http://cdimage.blankonlinux.or.id/blankon/livedvd-harian/current/tambora-desktop-$archi.iso.md5sum"
+wget "http://cdimage.blankonlinux.or.id/blankon/livedvd-harian/current/tambora-desktop-$archi.iso.sha1sum"
+wget "http://cdimage.blankonlinux.or.id/blankon/livedvd-harian/current/tambora-desktop-$archi.iso.sha256sum"
+
+
+echo "pengecekan md5sum ............ "
+md5sum -c "tambora-desktop-$archi.iso.md5sum"
+echo ""
+echo ""
+echo "pengecekan sha1sum ............ "
+sha1sum -c "tambora-desktop-$archi.iso.sha1sum"
+echo ""
+echo ""
+echo "pengecekan sha256sum ............ "
+sha256sum -c "tambora-desktop-$archi.iso.sha256sum"
+
+
+
